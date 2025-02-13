66ab5c0e8c30e211ce60ed80748c00f4e59ae70d
Caroline <c.jungefalk@gmail.com>
2025-02-13 12:31:48 +0100

    Lagt till händleselyssnare och funktion för sidinladdning

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+
+// Tom array för att lagra json-data
+let courses = [];
+
+//Händelsehanterare
+window.addEventListener("load", init);
+
+
+//Funktion för sidladdning
+function init(){
+
+    console.log("sidan har laddat klart")
+}
+
+
+
