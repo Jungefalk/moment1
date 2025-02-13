35bb3de788be6f077b06aea9e4e19e646cb4b68a
Caroline <c.jungefalk@gmail.com>
2025-02-13 12:42:54 +0100

    Hämtat in id från tabellen och skapat eventlyssnare

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,24 @@
 
 
 // Tom array för att lagra json-data
-let courses = [];
+let courseInfo = [];
+
+//Hämta in id och lagra i variabler
+let courseCodeEl = document.getElementById("courseCode");
+let courseNameEl = document.getElementById("courseName");
+let progressionEl = document.getElementById("progression");
 
 //Händelsehanterare
 window.addEventListener("load", init);
-
+courseCodeEl.addEventListener("click", sortCourseCode);
+courseNameEl.addEventListener("click", sortCourseName);
+progressionEl.addEventListener("click", sortProgression);
 
 //Funktion för sidladdning
 function init(){
 
     console.log("sidan har laddat klart")
-}
+};
 
 
 
