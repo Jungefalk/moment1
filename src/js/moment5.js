9e8ecf6c30ea1046990c3303477ff80a01581cf0
Caroline <c.jungefalk@gmail.com>
2025-02-27 20:39:09 +0100

    Hämtat in idn och skapat tomma arrayer

---
+++ src/js/moment5.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Hämta idn
+const barChartEl = document.getElementById("barChart");
+const pieChartEl = document.getElementById("pieChart")
+
+//Tomma arrayer för att lagra jsondata
+const mostPopularCourses = [];
+const mostPopularPrograms = [];
