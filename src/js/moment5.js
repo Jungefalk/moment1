4c534bb857e6b1d2d901cdbcafb41f1ca91ac347
Caroline <c.jungefalk@gmail.com>
2025-02-27 20:51:45 +0100

    Lagt till händelselyssnare

--- src/js/moment5.js
+++ src/js/moment5.js
@@ -2,8 +2,12 @@
 
 //Hämta idn
 const barChartEl = document.getElementById("barChart");
-const pieChartEl = document.getElementById("pieChart")
+const pieChartEl = document.getElementById("pieChart");
+
+//Händelselyssnare
+window.addEventListener("load", fetchData);
 
 //Tomma arrayer för att lagra jsondata
 const mostPopularCourses = [];
 const mostPopularPrograms = [];
+
