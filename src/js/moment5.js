55af1aeb4d19fa612338e19a14c14d70f28500bd
Caroline <c.jungefalk@gmail.com>
2025-02-28 10:51:02 +0100

    adderat sortfunktion

--- src/js/moment5.js
+++ src/js/moment5.js
@@ -21,12 +21,21 @@ async function fetchData() {
             throw new Error("Det blev ett fel vid anslutiningen");
         }
         const jsonData = await response.json()
+
+        // filtrera ut kurs/program
         const courses = jsonData.filter(data => data.type === "Kurs")
         console.log(courses);
 
         const programs = jsonData.filter(data => data.type === "Program")
         console.log(programs);
 
+        //stortera och plocka ut mest pupulära - lagra i array
+        const mostPopularCourses = courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal).slice(0, 6)
+        console.log(mostPopularCourses)
+
+        const mostPopularPrograms = programs.sort((a, b) => b.applicantsTotal - a.applicantsTotal).slice(0, 5)
+        console.log(mostPopularPrograms)
+        
 
     } catch (error) {
         console.log("Ett fel uppstod:", error.message);
