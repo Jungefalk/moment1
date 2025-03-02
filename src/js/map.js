0c14949fe071f16b7df757bd56614cca28f1fa29
Caroline <c.jungefalk@gmail.com>
2025-03-02 20:40:40 +0100

    Hämtat in idn

---
+++ src/js/map.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//Hämta idm
+
+let searchInputEl = document.getElementById("searchInput");
+let searchButtonEl = document.getElementById("searchButton");
+let mapEl = document.getElementById("map");
