3aa3e5d6f99fe5ff399f1051154167bd46b21f22
Caroline <c.jungefalk@gmail.com>
2025-03-03 21:25:29 +0100

    adderat händelselyssnare som laddar in kartan

--- src/js/map.js
+++ src/js/map.js
@@ -4,3 +4,20 @@
 
 let mapsInputEL = document.getElementById("mapsInput");
 let mapsButtonEL = document.getElementById("mapsButton")
+
+//Händelselyssnare
+window.addEventListener("load", initMap);
+
+let map;
+
+async function initMap() {
+
+    let map;
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
