7a6547bdb2a252ca6f888c68696a9d2452157bc9
Caroline <c.jungefalk@gmail.com>
2025-03-05 09:35:52 +0100

    Hämtat in idn

--- src/js/map.js
+++ src/js/map.js
@@ -1,5 +1,11 @@
 "use strict";
 
+//Hämta id:n
+let searchInputEl = document.getElementById("searchInput");
+let searchButtonEL = document.getElementById("searchButton");
+
+
+
 const map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
