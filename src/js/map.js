871633f66f4b618557738cf54225121b94ed1dfa
Caroline <c.jungefalk@gmail.com>
2025-03-06 11:01:23 +0100

    adderat händelselyssnare

--- src/js/map.js
+++ src/js/map.js
@@ -1,15 +1,18 @@
 "use strict";
 
 //Variabler 
-let searchedLocation = [];
+let searchedLocation = [59.325695, 18.071869];
 
 //Hämta id:n
 let searchInputEl = document.getElementById("searchInput");
 let searchButtonEL = document.getElementById("searchButton");
 
+//Händelsehanterare
+window.addEventListener("load", loadMap);
+searchButtonEL.addEventListener("click", fetchLocation);
 
 
-const map = L.map('map').setView([searchedLocation], 13);
+const map = L.map('map').setView(searchedLocation, 14);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
